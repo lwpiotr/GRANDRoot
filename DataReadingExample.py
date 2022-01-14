61f10219a8bb16d1337f0e7f010d045b09973d60
Lech Piotrowski <science@lwp.email>
2022-01-14 15:49:06 +0100

    Some updates in event readout

--- DataReadingExample.py
+++ DataReadingExample.py
@@ -4,42 +4,18 @@ import numpy as np
 from GRANDRootTrees import *
 # import ROOT
 
-# tadccounts = GRANDADCCountsTree("stored_data.root")
-#
-# tadccounts.GetEvent(2)
-# print(tadccounts.evt_id, tadccounts.det_time[0], tadccounts.trace_x[0])
+tadccounts = GRANDADCCountsTree("stored_data.root")
+
+tadccounts.GetEvent(2)
+print("ADCCounts readout: tadccounts.evt_id, tadccounts.det_time[0], tadccounts.trace_x[0]")
+print(tadccounts.evt_id, tadccounts.det_time[0], tadccounts.trace_x[0])
 
 
 tefield = GRANDEfieldTree("stored_data.root")
-tadccounts = GRANDADCCountsTree("stored_data.root")
-tvoltage = GRANDVoltageTree("stored_data.root")
-#tefield = GRANDADCCountsTree("stored_data.root")
+#tvoltage = GRANDVoltageTree("stored_data.root")
 
 tefield.GetEvent(2)
-print(tefield.evt_id, tefield.det_time[0], tefield.trace_x[0][0])#, tadccounts)
-#print("post")
+print("\nEfield readout: tefield.evt_id, tefield.det_time[0], tefield.trace_x[0][0], tadccounts.evt_id")
+print("The evt_id of tadccounts changed to 4 when tefield event with evt_id 4 was requested")
+print(tefield.evt_id, tefield.det_time[0], tefield.trace_x[0][0], tadccounts.evt_id)
 
-# Here are experiments to avoid the crash - not working yet!
-
-for tree in tefield.tree.GetListOfFriends():
-	tefield.tree.RemoveFriend(tree.GetTree())
-
-for tree in tadccounts.tree.GetListOfFriends():
-	tadccounts.tree.RemoveFriend(tree.GetTree())
-	
-for tree in tvoltage.tree.GetListOfFriends():
-	tvoltage.tree.RemoveFriend(tree.GetTree())
-
-tadccounts.tree.Delete()
-tvoltage.tree.Delete()
-tefield.tree.Delete()
-#tefield.file.Close()
-print("aaa")
-
-#tefield.RemoveFriend(tadccounts.tree)
-#print(tadccounts.tree.GetListOfFriends())
-#exit()
-for tree in tefield.tree.GetListOfFriends():
-	print(tree)
-#tefield.RemoveFriend(tvoltage.tree)
-#tvoltage.RemoveFriend(tadccounts.tree)
